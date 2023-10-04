bcd5b6f1f6c29974993dbf787fe916f4ba3569a0
ArkamaHozota <arkamahozota@gmail.com>
2023-10-04 10:37:35 +0900

    2.66

--- src/main/java/jp/co/sbsssmcrud/ppog/controller/SbsSsmCrudController.java
+++ src/main/java/jp/co/sbsssmcrud/ppog/controller/SbsSsmCrudController.java
@@ -2,12 +2,6 @@ package jp.co.sbsssmcrud.ppog.controller;
 
 import java.util.List;
 
-import jp.co.sbsssmcrud.ppog.dto.CityDto;
-import jp.co.sbsssmcrud.ppog.service.SbsSsmCrudLogicService;
-import jp.co.sbsssmcrud.ppog.utils.Messages;
-import jp.co.sbsssmcrud.ppog.utils.Pagination;
-import jp.co.sbsssmcrud.ppog.utils.RestMsg;
-import jp.co.sbsssmcrud.ppog.utils.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jp.co.sbsssmcrud.ppog.dto.CityDto;
+import jp.co.sbsssmcrud.ppog.service.SbsSsmCrudLogicService;
+import jp.co.sbsssmcrud.ppog.utils.Messages;
+import jp.co.sbsssmcrud.ppog.utils.Pagination;
+import jp.co.sbsssmcrud.ppog.utils.RestMsg;
+import jp.co.sbsssmcrud.ppog.utils.StringUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -38,63 +38,20 @@ public class SbsSsmCrudController {
 	private final SbsSsmCrudLogicService sbsSsmCrudLogicService;
 
 	/**
-	 * 都市情報を検索する
+	 * 入力した都市名を重複かどうかをチェックする
 	 *
-	 * @return modelAndView
-	 */
-	@GetMapping(value = "/city")
-	public RestMsg getCityInfo(@RequestParam(value = "pageNum", defaultValue = "1") final Integer pageNum,
-                               @RequestParam(value = "keyword", defaultValue = StringUtils.EMPTY_STRING) final String keyword) {
-		// ページング検索結果を吹き出します；
-		final Pagination<CityDto> pageInfo = this.sbsSsmCrudLogicService.getPageInfo(pageNum, keyword);
-		return RestMsg.success().add("pageInfo", pageInfo);
-	}
-
-	/**
-	 * 指定された都市の情報を取得する
-	 *
-	 * @param id 都市ID
-	 * @return 都市情報
-	 */
-	@GetMapping(value = "/city/{id}")
-	public RestMsg getCityInfo(@PathVariable("id") final Integer id) {
-		final CityDto cityDto = this.sbsSsmCrudLogicService.getCityInfoById(id);
-		return RestMsg.success().add("citySelected", cityDto);
-	}
-
-	/**
-	 * 指定された都市の大陸に位置するすべての国を取得する
-	 *
-	 * @param id 都市ID
-	 * @return 国のリスト
-	 */
-	@GetMapping(value = "/countries/{id}")
-	public RestMsg getListOfNationsById(@PathVariable("id") final Integer id) {
-		final List<String> nations = this.sbsSsmCrudLogicService.getListOfNationsById(id);
-		return RestMsg.success().add("nationsByName", nations);
-	}
-
-	/**
-	 * 入力した都市情報を変更する
-	 *
-	 * @param cityDto 都市情報DTO
+	 * @param cityName 都市名称
 	 * @return 処理成功のメッセージ
 	 */
-	@PutMapping(value = "/city/{id}")
-	public RestMsg updateCityInfo(@RequestBody final CityDto cityDto) {
-		this.sbsSsmCrudLogicService.update(cityDto);
-		return RestMsg.success();
-	}
-
-	/**
-	 * 入力した都市情報を保存する
-	 *
-	 * @param cityDto 都市情報DTO
-	 * @return 処理成功のメッセージ
-	 */
-	@PostMapping(value = "/city")
-	public RestMsg saveCityInfo(@RequestBody final CityDto cityDto) {
-		this.sbsSsmCrudLogicService.save(cityDto);
+	@GetMapping(value = "/check")
+	public RestMsg checkName(@RequestParam("cityName") final String cityName) {
+		if (!cityName.matches(Messages.MSG006)) {
+			return RestMsg.failure().add("validatedMsg", Messages.MSG005);
+		}
+		final Integer checkInteger = this.sbsSsmCrudLogicService.checkDuplicate(cityName);
+		if (checkInteger > 0) {
+			return RestMsg.failure().add("validatedMsg", Messages.MSG004);
+		}
 		return RestMsg.success();
 	}
 
@@ -111,6 +68,31 @@ public class SbsSsmCrudController {
 	}
 
 	/**
+	 * 指定された都市の情報を取得する
+	 *
+	 * @param id 都市ID
+	 * @return 都市情報
+	 */
+	@GetMapping(value = "/city/{id}")
+	public RestMsg getCityInfo(@PathVariable("id") final Integer id) {
+		final CityDto cityDto = this.sbsSsmCrudLogicService.getCityInfoById(id);
+		return RestMsg.success().add("citySelected", cityDto);
+	}
+
+	/**
+	 * 都市情報を検索する
+	 *
+	 * @return modelAndView
+	 */
+	@GetMapping(value = "/city")
+	public RestMsg getCityInfo(@RequestParam(value = "pageNum", defaultValue = "1") final Integer pageNum,
+			@RequestParam(value = "keyword", defaultValue = StringUtils.EMPTY_STRING) final String keyword) {
+		// ページング検索結果を吹き出します；
+		final Pagination<CityDto> pageInfo = this.sbsSsmCrudLogicService.getPageInfo(pageNum, keyword);
+		return RestMsg.success().add("pageInfo", pageInfo);
+	}
+
+	/**
 	 * 大陸情報を取得する
 	 *
 	 * @return 大陸名称のリスト
@@ -119,18 +101,6 @@ public class SbsSsmCrudController {
 	public RestMsg getContinents() {
 		final List<String> continents = this.sbsSsmCrudLogicService.findAllContinents();
 		return RestMsg.success().add("continents", continents);
-	}
-
-	/**
-	 * 指定された大陸に位置するすべての国を取得する
-	 *
-	 * @param continentVal 大陸名称
-	 * @return 国のリスト
-	 */
-	@GetMapping(value = "/countries")
-	public RestMsg getListOfNationsById(@RequestParam("continentVal") final String continentVal) {
-		final List<String> nations = this.sbsSsmCrudLogicService.findNationsByCnt(continentVal);
-		return RestMsg.success().add("nations", nations);
 	}
 
 	/**
@@ -146,20 +116,50 @@ public class SbsSsmCrudController {
 	}
 
 	/**
-	 * 入力した都市名を重複かどうかをチェックする
+	 * 指定された都市の大陸に位置するすべての国を取得する
 	 *
-	 * @param cityName 都市名称
+	 * @param id 都市ID
+	 * @return 国のリスト
+	 */
+	@GetMapping(value = "/countries/{id}")
+	public RestMsg getListOfNationsById(@PathVariable("id") final Integer id) {
+		final List<String> nations = this.sbsSsmCrudLogicService.getListOfNationsById(id);
+		return RestMsg.success().add("nationsByName", nations);
+	}
+
+	/**
+	 * 指定された大陸に位置するすべての国を取得する
+	 *
+	 * @param continentVal 大陸名称
+	 * @return 国のリスト
+	 */
+	@GetMapping(value = "/countries")
+	public RestMsg getListOfNationsById(@RequestParam("continentVal") final String continentVal) {
+		final List<String> nations = this.sbsSsmCrudLogicService.findNationsByCnt(continentVal);
+		return RestMsg.success().add("nations", nations);
+	}
+
+	/**
+	 * 入力した都市情報を保存する
+	 *
+	 * @param cityDto 都市情報DTO
 	 * @return 処理成功のメッセージ
 	 */
-	@GetMapping(value = "/check")
-	public RestMsg checkName(@RequestParam("cityName") final String cityName) {
-		if (!cityName.matches(Messages.MSG006)) {
-			return RestMsg.failure().add("validatedMsg", Messages.MSG005);
-		}
-		final Integer checkInteger = this.sbsSsmCrudLogicService.checkDuplicate(cityName);
-		if (checkInteger > 0) {
-			return RestMsg.failure().add("validatedMsg", Messages.MSG004);
-		}
+	@PostMapping(value = "/city")
+	public RestMsg saveCityInfo(@RequestBody final CityDto cityDto) {
+		this.sbsSsmCrudLogicService.save(cityDto);
+		return RestMsg.success();
+	}
+
+	/**
+	 * 入力した都市情報を変更する
+	 *
+	 * @param cityDto 都市情報DTO
+	 * @return 処理成功のメッセージ
+	 */
+	@PutMapping(value = "/city/{id}")
+	public RestMsg updateCityInfo(@RequestBody final CityDto cityDto) {
+		this.sbsSsmCrudLogicService.update(cityDto);
 		return RestMsg.success();
 	}
 }
