9a19458415acbe6bffc2d8bfe5ac381c236c4d0e
ArkamaHozota <arkamahozota@gmail.com>
2023-10-04 10:37:03 +0900

    2.64

--- src/main/java/jp/co/sbsssmcrud/ppog/config/WebMvcConfig.java
+++ src/main/java/jp/co/sbsssmcrud/ppog/config/WebMvcConfig.java
@@ -2,13 +2,13 @@ package jp.co.sbsssmcrud.ppog.config;
 
 import java.util.List;
 
-import jp.co.sbsssmcrud.ppog.utils.Messages;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import jp.co.sbsssmcrud.ppog.utils.Messages;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -22,30 +22,30 @@ import lombok.extern.log4j.Log4j2;
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
 	/**
+	 * 静的リソースマッピングを設定する
+	 *
+	 * @param registry 注冊説明
+	 */
+	@Override
+	protected void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		WebMvcConfig.log.info(Messages.MSG002);
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
+	}
+
+	/**
 	 * SpringMVCフレームワークを拡張するメッセージ・コンバーター
 	 *
 	 * @param converters コンバーター
 	 */
 	@Override
 	protected void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
-		log.info(Messages.MSG001);
-		// 創建消息轉換器對象；
+		WebMvcConfig.log.info(Messages.MSG001);
+		// メッセージコンバータオブジェクトを作成する。
 		final MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-		// 設置對象轉換器，底層使用Jackson將Java對象轉為JSON；
+		// オブジェクトコンバータを設定し、Jacksonを使用してJavaオブジェクトをJSONに変換します。
 		messageConverter.setObjectMapper(new JacksonObjectMapper());
-		// 將上述消息轉換器追加到SpringMVC框架的轉換器容器中；
+		// 上記のメッセージコンバータをSpringMVCフレームワークのコンバータコンテナに追加します。
 		converters.add(0, messageConverter);
-	}
-
-	/**
-	 * 設置靜態資源映射
-	 *
-	 * @param registry 注冊説明
-	 */
-	@Override
-	protected void addResourceHandlers(final ResourceHandlerRegistry registry) {
-		log.info(Messages.MSG002);
-		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-		registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
 	}
 }
