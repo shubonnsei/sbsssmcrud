bab05320987d5b980e5918fbf77dbdf4c0fe20e0
ArkamaHozota <arkamahozota@gmail.com>
2023-10-04 10:37:14 +0900

    2.65

--- src/main/java/jp/co/sbsssmcrud/ppog/mapper/CityMapper.java
+++ src/main/java/jp/co/sbsssmcrud/ppog/mapper/CityMapper.java
@@ -17,6 +17,85 @@ import jp.co.sbsssmcrud.ppog.entity.City;
 public interface CityMapper {
 
 	/**
+	 * 都市名は重複するかどうかを判断する
+	 *
+	 * @param cityName 都市名
+	 * @return Integer
+	 */
+	Integer checkDuplicatedName(@Param("cityName") String cityName);
+
+	/**
+	 * すべての都市情報のレコード数を取得する
+	 *
+	 * @return Integer
+	 */
+	Integer countCityInfos();
+
+	/**
+	 * 都市名によって都市情報のレコード数を取得する
+	 *
+	 * @param name 都市名
+	 * @return Integer
+	 */
+	Integer countCityInfosByName(@Param("name") String name);
+
+	/**
+	 * 国名によって都市情報のレコード数を取得する
+	 *
+	 * @param nationCode
+	 * @return Integer
+	 */
+	Integer countCityInfosByNation(@Param("nation") String nationCode);
+
+	/**
+	 * 人口数量降順で都市情報を検索する
+	 *
+	 * @param sortNumber ソート
+	 * @return List<City>
+	 */
+	List<City> findMaximumRanks(@Param("sort") Integer sortNumber);
+
+	/**
+	 * 人口数量昇順で都市情報を検索する
+	 *
+	 * @param sortNumber ソート
+	 * @return List<City>
+	 */
+	List<City> findMinimumRanks(@Param("sort") Integer sortNumber);
+
+	/**
+	 * すべての都市情報を取得する
+	 *
+	 * @return List<City>
+	 */
+	List<City> getCityInfos(@Param("offset") Integer offset, @Param("pageSize") Integer pageSize);
+
+	/**
+	 * 都市名によって都市情報を取得する
+	 *
+	 * @param name 都市名
+	 * @return List<City>
+	 */
+	List<City> getCityInfosByName(@Param("name") String name, @Param("offset") Integer offset,
+			@Param("pageSize") Integer pageSize);
+
+	/**
+	 * 国名によって都市情報を取得する
+	 *
+	 * @param nationCode
+	 * @return Integer
+	 */
+	List<City> getCityInfosByNation(@Param("nation") String nationCode, @Param("offset") Integer offset,
+			@Param("pageSize") Integer pageSize);
+
+	/**
+	 * 論理削除
+	 *
+	 * @param id id of the selected city
+	 */
+	void removeById(@Param("id") Integer id);
+
+	/**
 	 * 採番を行います
 	 *
 	 * @return 採番値
@@ -44,83 +123,4 @@ public interface CityMapper {
 	 * @param city 都市エンティティ
 	 */
 	void updateById(City city);
-
-	/**
-	 * 論理削除
-	 *
-	 * @param id id of the selected city
-	 */
-	void removeById(@Param("id") Integer id);
-
-	/**
-	 * 人口数量降順で都市情報を検索する
-	 *
-	 * @param sortNumber ソート
-	 * @return List<City>
-	 */
-	List<City> findMaximumRanks(@Param("sort") Integer sortNumber);
-
-	/**
-	 * 人口数量昇順で都市情報を検索する
-	 *
-	 * @param sortNumber ソート
-	 * @return List<City>
-	 */
-	List<City> findMinimumRanks(@Param("sort") Integer sortNumber);
-
-	/**
-	 * 国名によって都市情報を取得する
-	 *
-	 * @param nationCode
-	 * @return Integer
-	 */
-	List<City> getCityInfosByNation(@Param("nation") String nationCode, @Param("offset") Integer offset,
-			@Param("pageSize") Integer pageSize);
-
-	/**
-	 * 国名によって都市情報のレコード数を取得する
-	 *
-	 * @param nationCode
-	 * @return Integer
-	 */
-	Integer countCityInfosByNation(@Param("nation") String nationCode);
-
-	/**
-	 * 都市名によって都市情報を取得する
-	 *
-	 * @param name 都市名
-	 * @return List<City>
-	 */
-	List<City> getCityInfosByName(@Param("name") String name, @Param("offset") Integer offset,
-			@Param("pageSize") Integer pageSize);
-
-	/**
-	 * 都市名によって都市情報のレコード数を取得する
-	 *
-	 * @param name 都市名
-	 * @return Integer
-	 */
-	Integer countCityInfosByName(@Param("name") String name);
-
-	/**
-	 * すべての都市情報を取得する
-	 *
-	 * @return List<City>
-	 */
-	List<City> getCityInfos(@Param("offset") Integer offset, @Param("pageSize") Integer pageSize);
-
-	/**
-	 * すべての都市情報のレコード数を取得する
-	 *
-	 * @return Integer
-	 */
-	Integer countCityInfos();
-
-	/**
-	 * 都市名は重複するかどうかを判断する
-	 *
-	 * @param cityName 都市名
-	 * @return Integer
-	 */
-	Integer checkDuplicatedName(@Param("cityName") String cityName);
 }
--- src/main/java/jp/co/sbsssmcrud/ppog/mapper/CountryMapper.java
+++ src/main/java/jp/co/sbsssmcrud/ppog/mapper/CountryMapper.java
@@ -22,18 +22,18 @@ public interface CountryMapper {
 	List<String> findAllContinents();
 
 	/**
-	 * 選択された大陸の上にすべての国の情報を取得する
-	 *
-	 * @param continent 大陸名
-	 * @return List<String>
-	 */
-	List<String> findNationsByCnt(@Param("continent") String continent);
-
-	/**
 	 * 国名によって国家コードを抽出する
 	 *
 	 * @param nationVal 国名
 	 * @return String
 	 */
 	String findNationCode(@Param("nation") String nationVal);
+
+	/**
+	 * 選択された大陸の上にすべての国の情報を取得する
+	 *
+	 * @param continent 大陸名
+	 * @return List<String>
+	 */
+	List<String> findNationsByCnt(@Param("continent") String continent);
 }
