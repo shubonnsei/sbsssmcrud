a711405d12f6e3552f4f73cefe3aebea402e732a
ArkamaHozota <arkamahozota@gmail.com>
2023-10-20 17:38:16 +0900

    2.93

--- src/main/resources/static/jquery/sbscitinavi.js
+++ src/main/resources/static/jquery/sbscitinavi.js
@@ -293,22 +293,7 @@ function getCityInfo(id) {
 			$("#languageEdit").text(cityData.language);
 			$("#districtEdit").val(cityData.district);
 			$("#populationEdit").val(cityData.population);
-			getNationsById("#nationEdit", id);
-		}
-	});
-}
-
-// Get the name of country.
-function getNationsById(element, id) {
-	$(element).empty();
-	$.ajax({
-		url: pathdeApp + '/countries/' + id,
-		type: 'GET',
-		success: function(result) {
-			$.each(result.extend.nationsByName, function() {
-				let optionElement = $("<option></option>").append(this).attr("value", this);
-				optionElement.appendTo(element);
-			});
+			getNations("#nationEdit", id);
 		}
 	});
 }
