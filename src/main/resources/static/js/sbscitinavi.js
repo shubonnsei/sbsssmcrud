29e12fe303e27c3933edfcd1f4b317e1442ebe00
ArkamaHozota <arkamahozota@hotmail.com>
2024-04-12 13:33:07 +0900

    2.99

--- src/main/resources/static/js/sbscitinavi.js
+++ src/main/resources/static/js/sbscitinavi.js
@@ -1,13 +1,13 @@
 const pathdeApp = "ssmcrud";
-let currentPage = 1;
-let totalRecord, totalPages, searchName;
-
-// After the page load, send the ajax request to get page infos.
+let pageNum = 1;
+let totalRecords, totalPages, searchName;
 $(document).ready(function() {
-	toSelectedPg(currentPage, searchName);
+	toSelectedPg(pageNum, searchName);
 });
-
-// Transfer to clicked page numbers.
+$("#searchBtn").on('click', function() {
+	searchName = $("#keywordInput").val().trim().toString();
+	toSelectedPg(1, searchName);
+});
 function toSelectedPg(pageNum, searchName) {
 	$.ajax({
 		url: pathdeApp + '/city',
@@ -23,10 +23,7 @@ function toSelectedPg(pageNum, searchName) {
 		}
 	})
 }
-
-// Analyze and display the data.
 function buildCityTable(result) {
-	// Emptying the former table.
 	$("#cityTableBody").empty();
 	let index = result.extend.pageInfo.records;
 	$.each(index, (index, item) => {
@@ -64,34 +61,30 @@ function buildCityTable(result) {
 			languageTd = $("<td class='text-center' style='width:80px;font-size:15px;vertical-align:bottom;'></td>").append(languageName);
 		}
 		let editBtn = $("<button></button>").addClass("btn btn-primary btn-sm edit_btn")
-			.append($("<i class='bi bi-pencil-fill'></i>")).append("編集");
+			.append($("<i class='fa-solid fa-pencil'></i>")).append("編集");
 		editBtn.attr("editId", item.id);
 		let deleteBtn = $("<button></button>").addClass("btn btn-danger btn-sm delete_btn")
-			.append($("<i class='bi bi-trash'></i>")).append("削除");
+			.append($("<i class='fa-solid fa-trash'></i>")).append("削除");
 		deleteBtn.attr("deleteId", item.id);
 		let btnTd = $("<td class='text-center' style='width:120px;vertical-align:bottom;'></td>").append(editBtn).append(" ").append(deleteBtn);
 		$("<tr></tr>").append(idTd).append(nameTd).append(continentTd).append(nationTd).append(districtTd).append(populationTd).append(languageTd)
 			.append(btnTd).appendTo("#cityTableBody");
 	});
 }
-
-// Analyze and display the page infos.
 function buildPageInfos(result) {
 	let pageInfos = $("#pageInfos");
 	pageInfos.empty();
-	currentPage = result.extend.pageInfo.pageNum;
+	pageNum = result.extend.pageInfo.pageNum;
 	totalPages = result.extend.pageInfo.totalPages;
-	totalRecord = result.extend.pageInfo.totalRecords;
-	pageInfos.append("The " + currentPage + " page in " + totalPages
-		+ " pages, " + totalRecord + " records found.");
+	totalRecords = result.extend.pageInfo.totalRecords;
+	pageInfos.append("The " + pageNum + " page in " + totalPages
+		+ " pages, " + totalRecords + " records found.");
 }
-
-// Analyze and display the navigated pages.
 function buildPageNavi(result) {
 	$("#pageNavi").empty();
 	let ul = $("<ul></ul>").addClass("pagination");
 	let firstPageLi = $("<li class='page-item'></li>").append(
-		$("<a class='page-link'></a>").append("最初のページへ").attr("href", "#"));
+		$("<a class='page-link'></a>").append("最初へ").attr("href", "#"));
 	let previousPageLi = $("<li class='page-item'></li>").append(
 		$("<a class='page-link'></a>").append("&laquo;").attr("href", "#"));
 	if (!result.extend.pageInfo.hasPreviousPage) {
@@ -102,20 +95,20 @@ function buildPageNavi(result) {
 			toSelectedPg(1, searchName);
 		});
 		previousPageLi.click(function() {
-			toSelectedPg(currentPage - 1, searchName);
+			toSelectedPg(pageNum - 1, searchName);
 		});
 	}
 	let nextPageLi = $("<li class='page-item'></li>").append(
 		$("<a class='page-link'></a>").append("&raquo;").attr("href", "#"));
 	let lastPageLi = $("<li class='page-item'></li>").append(
-		$("<a class='page-link'></a>").append("最後のページへ").attr("href", "#"));
+		$("<a class='page-link'></a>").append("最後へ").attr("href", "#"));
 	if (!result.extend.pageInfo.hasNextPage) {
 		nextPageLi.addClass("disabled");
 		lastPageLi.addClass("disabled");
 	} else {
 		lastPageLi.addClass("success");
 		nextPageLi.click(function() {
-			toSelectedPg(currentPage + 1, searchName);
+			toSelectedPg(pageNum + 1, searchName);
 		});
 		lastPageLi.click(function() {
 			toSelectedPg(totalPages, searchName);
@@ -125,7 +118,7 @@ function buildPageNavi(result) {
 	$.each(result.extend.pageInfo.navigatePageNums, (index, item) => {
 		let numsLi = $("<li class='page-item'></li>").append(
 			$("<a class='page-link'></a>").append(item).attr("href", "#"));
-		if (currentPage === item) {
+		if (pageNum === item) {
 			numsLi.attr("href", "#").addClass("active");
 		}
 		numsLi.click(function() {
@@ -137,82 +130,65 @@ function buildPageNavi(result) {
 	let navElement = $("<nav></nav>").append(ul);
 	navElement.appendTo("#pageNavi");
 }
-
-// Add click function to search btn.
-$("#searchBtn").on('click', function() {
-	searchName = $("#keywordInput").val().trim().toString();
-	toSelectedPg(1, searchName);
-});
-
-// Add click function to the input modal.
 $("#cityAddModalBtn").on('click', function() {
 	formReset("#cityAddModal form");
-	getContinent("#continentInput");
+	getContinents("#continentInput");
 	getNations($("#nationInput"), 'Africa');
-	$("#cityAddModal").modal({
+	let addModal = new bootstrap.Modal($("#cityAddModal"), {
 		backdrop: 'static'
 	});
+	addModal.show();
 });
-
-// Add click function to select of continent.
-$("#continentInput").on('change', function() {
-	let continentVal = $("#continentInput option:selected").val();
-	getNations($("#nationInput"), continentVal);
-})
-
-// Get the name of continent.
-function getContinent(element) {
+function getContinents(element) {
 	$(element).empty();
 	$.ajax({
-		url: pathdeApp + '/continents',
+		url: '/jpasample/continents',
 		type: 'GET',
+		dataType: 'json',
 		success: function(result) {
-			$.each(result.extend.continents, function() {
-				let optionElement = $("<option></option>").append(this).attr(
-					"value", this);
-				optionElement.appendTo(element);
-			});
-		}
-	});
-}
-
-// Get the name of country.
-function getNations(element, continentVal) {
-	$(element).empty();
-	$.ajax({
-		url: pathdeApp + '/countries',
-		data: 'continentVal=' + continentVal,
-		type: 'GET',
-		success: function(result) {
-			$.each(result.extend.nations, function() {
+			$.each(result.extend.continentList, function() {
 				let optionElement = $("<option></option>").append(this).attr("value", this);
 				optionElement.appendTo(element);
 			});
 		}
 	});
 }
-
-// Analyze the input name is duplicate or not.
-$("#nameInput").change(
-	function() {
-		let cityName = this.value;
-		$.ajax({
-			url: pathdeApp + '/checklist',
-			data: 'cityName=' + cityName,
-			type: 'GET',
-			success: function(result) {
-				if (result.code === 200) {
-					showValidationMsg("#nameInput", "success", "");
-					$("#cityInfoSaveBtn").attr("ajax-va", "success");
-				} else {
-					showValidationMsg("#nameInput", "error", result.extend.validatedMsg);
-					$("#cityInfoSaveBtn").attr("ajax-va", "error");
-				}
-			}
-		});
+$("#continentInput").on('change', function() {
+	let continentVal = $("#continentInput option:selected").val();
+	getNations($("#nationInput"), continentVal);
+});
+function getNations(element, continentVal) {
+	$(element).empty();
+	$.ajax({
+		url: '/jpasample/nations',
+		data: 'continentVal=' + continentVal,
+		type: 'GET',
+		dataType: 'json',
+		success: function(result) {
+			$.each(result.extend.nationList, function() {
+				let optionElement = $("<option></option>").append(this).attr("value", this);
+				optionElement.appendTo(element);
+			});
+		}
 	});
-
-// Click, save and check the input data format.
+}
+$("#nameInput").change(function() {
+	let cityName = this.value;
+	$.ajax({
+		url: '/jpasample/check',
+		data: 'cityName=' + cityName,
+		type: 'GET',
+		success: function(result) {
+			if (result.code === 200) {
+				showValidationMsg("#nameInput", "success", "");
+				$("#infoSaveBtn").attr("ajax-va", "success");
+			} else {
+				showValidationMsg("#nameInput", "error", result.extend.validatedMsg);
+				$("#infoSaveBtn").attr("ajax-va", "error");
+			}
+		}
+	});
+});
 $("#cityInfoSaveBtn").on('click', function() {
 	let inputDistrict = $("#districtInput").val().trim();
 	let inputPopulation = $("#populationInput").val().trim();
@@ -222,70 +198,60 @@ $("#cityInfoSaveBtn").on('click', function() {
 		return false;
 	} else if (!regularDistrict.test(inputDistrict)
 		&& !regularPopulation.test(inputPopulation)) {
-		showValidationMsg("#districtInput", "error",
-			"Districts' name should be in 2~33 Latin alphabets.");
-		showValidationMsg("#populationInput", "error",
-			"Population should be in 4~18 numbers.");
+		showValidationMsg("#districtInput", "error", "入力した地域名称が2桁から23桁までのローマ字にしなければなりません。");
+		showValidationMsg("#populationInput", "error", "入力した人口数量が4桁から18桁までの数字にしなければなりません。");
 		return false;
 	} else if (!regularDistrict.test(inputDistrict)
 		&& regularPopulation.test(inputPopulation)) {
-		showValidationMsg("#districtInput", "error",
-			"Districts' name should be in 2~33 Latin alphabets.");
+		showValidationMsg("#districtInput", "error", "入力した地域名称が2桁から23桁までのローマ字にしなければなりません。");
 		showValidationMsg("#populationInput", "success", "√");
 		return false;
 	} else if (regularDistrict.test(inputDistrict)
 		&& !regularPopulation.test(inputPopulation)) {
 		showValidationMsg("#districtInput", "success", "√");
-		showValidationMsg("#populationInput", "error",
-			"Population should be in 4~18 numbers.");
+		showValidationMsg("#populationInput", "error", "入力した人口数量が4桁から18桁までの数字にしなければなりません。");
 		return false;
 	} else {
 		showValidationMsg("#districtInput", "success", "√");
 		showValidationMsg("#populationInput", "success", "√");
-		// Send an ajax request to commit save options.
 		$.ajax({
-			url: pathdeApp + '/city',
+			url: '/jpasample/city',
 			type: 'POST',
-			contentType: 'application/json;charset=UTF-8',
 			dataType: 'json',
-			data: JSON
-				.stringify({
-					'name': $("#nameInput").val().trim(),
-					'continent': $("#continentInput option:selected").val(),
-					'nation': $("#nationInput option:selected").val(),
-					'district': inputDistrict,
-					'population': inputPopulation
-				}),
+			data: JSON.stringify({
+				'name': $("#nameInput").val().trim(),
+				'continent': $("#continentInput option:selected").val(),
+				'nation': $("#nationInput option:selected").val(),
+				'district': inputDistrict,
+				'population': inputPopulation
+			}),
+			contentType: 'application/json;charset=UTF-8',
 			success: function(result) {
 				if (result.code === 200) {
 					$("#cityAddModal").modal('hide');
-					// To last page.
-					toSelectedPg(totalPages, searchName);
-				} else if (undefined !== result.extend.errorFields.name) {
-					showValidationMsg("#nameInput", "error", result.extend.errorFields.name);
+					window.location
+						.replace('/jpasample/city?pageNum=' + totalPages + '&keyword=');
+				} else {
+					layer.msg(result.extend.errorMsg);
 				}
 			}
 		});
 	}
 });
-
-// Add click function to the edit modal.
 $(document).on('click', '.edit_btn', function() {
 	let editId = $(this).attr("editId");
 	formReset("#cityEditModal form");
 	getCityInfo(editId);
 	$("#cityInfoChangeBtn").attr("editId", editId);
-	$("#cityEditModal").modal({
+	let editModal = new bootstrap.Modal($("#cityEditModal"), {
 		backdrop: 'static'
 	});
+	editModal.show();
 });
-
-// Get the selected city infos.
 function getCityInfo(id) {
 	$.ajax({
-		url: pathdeApp + '/city/' + id,
+		url: '/jpasample/city/' + id,
 		type: 'GET',
-		dataType: 'json',
 		success: function(result) {
 			let cityData = result.extend.citySelected;
 			$("#nameEdit").text(cityData.name);
@@ -297,61 +263,51 @@ function getCityInfo(id) {
 		}
 	});
 }
-
-// Add click function to select of nation.
 $("#nationEdit").on('change', function() {
 	let nationVal = $("#nationEdit option:selected").val();
-	getLanguage($("#languageEdit"), nationVal);
-})
-
-// Get the name of country.
-function getLanguage(element, nationVal) {
+	getLanguages($("#languageEdit"), nationVal);
+});
+function getLanguages(element, nationVal) {
 	$(element).empty();
 	$.ajax({
-		url: pathdeApp + '/language',
+		url: '/jpasample/languages',
 		data: 'nationVal=' + nationVal,
 		type: 'GET',
+		dataType: 'json',
 		success: function(result) {
 			$("#languageEdit").text(result.extend.languages);
 		}
 	});
 }
-
-// Submit the change of city infos.
 $("#cityInfoChangeBtn").on('click', function() {
+	let editId = $(this).attr("editId");
 	let inputDistrict = $("#districtEdit").val().trim();
 	let inputPopulation = $("#populationEdit").val().trim();
 	let regularDistrict = /^[a-zA-Z-\s]{2,33}$/;
 	let regularPopulation = /^\d{4,18}$/;
-	let editId = $(this).attr("editId");
 	if (!regularDistrict.test(inputDistrict)
 		&& !regularPopulation.test(inputPopulation)) {
-		showValidationMsg("#districtEdit", "error",
-			"Districts' name should be in 2~33 Latin alphabets.");
-		showValidationMsg("#populationEdit", "error",
-			"Population should be in 4~18 numbers.");
+		showValidationMsg("#districtEdit", "error", "入力した地域名称が2桁から23桁までのローマ字にしなければなりません。");
+		showValidationMsg("#populationEdit", "error", "入力した人口数量が4桁から18桁までの数字にしなければなりません。");
 		return false;
 	}
 	if (!regularDistrict.test(inputDistrict)
 		&& regularPopulation.test(inputPopulation)) {
-		showValidationMsg("#districtEdit", "error",
-			"Districts' name should be in 2~33 Latin alphabets.");
+		showValidationMsg("#districtEdit", "error", "入力した地域名称が2桁から23桁までのローマ字にしなければなりません。");
 		showValidationMsg("#populationEdit", "success", "√");
 		return false;
 	}
 	if (regularDistrict.test(inputDistrict)
 		&& !regularPopulation.test(inputPopulation)) {
 		showValidationMsg("#districtEdit", "success", "√");
-		showValidationMsg("#populationEdit", "error",
-			"Population should be in 4~18 numbers.");
+		showValidationMsg("#populationEdit", "error", "入力した人口数量が4桁から18桁までの数字にしなければなりません。");
 		return false;
 	}
 	showValidationMsg("#districtEdit", "success", "√");
 	showValidationMsg("#populationEdit", "success", "√");
 	$.ajax({
-		url: pathdeApp + '/city/' + editId,
+		url: '/jpasample/city/' + editId,
 		type: 'PUT',
-		contentType: 'application/json;charset=UTF-8',
 		dataType: 'json',
 		data: JSON.stringify({
 			'id': editId,
@@ -361,37 +317,52 @@ $("#cityInfoChangeBtn").on('click', function() {
 			'district': inputDistrict,
 			'population': inputPopulation
 		}),
-		success: function() {
-			$("#cityEditModal").modal('hide');
-			toSelectedPg(currentPage, searchName);
+		contentType: 'application/json;charset=UTF-8',
+		success: function(result) {
+			if (result.code === 200) {
+				$("#cityEditModal").modal('hide');
+				toSelectedPg(pageNum, searchName);
+				layer.msg(result.message);
+			} else {
+				layer.msg(result.extend.errorMsg);
+			}
 		}
 	});
 });
-
-// Add click function to the delete modal.
 $(document).on('click', '.delete_btn', function() {
 	let cityName = $(this).parents("tr").find("td:eq(0)").text().trim();
 	let cityId = $(this).attr("deleteId");
-	if (confirm("この" + cityName + "という都市の情報を削除する、よろしいでしょうか。")) {
-		$.ajax({
-			url: pathdeApp + "/city/" + cityId,
-			type: "DELETE",
-			success: function() {
-				toSelectedPg(currentPage, searchName);
-			}
-		});
-	}
+	swal.fire({
+		title: 'メッセージ',
+		text: 'この「' + cityName + '」という都市の情報を削除する、よろしいでしょうか。',
+		icon: 'question',
+		showDenyButton: true,
+		denyButtonText: 'いいえ',
+		denyButtonColor: '#002FA7',
+		confirmButtonText: 'はい',
+		confirmButtonColor: '#7F0020'
+	}).then((result) => {
+		if (result.isConfirmed) {
+			$.ajax({
+				url: pathdeApp + '/cityDel',
+				data: 'cityId=' + cityId,
+				type: 'DELETE',
+				success: function(result) {
+					toSelectedPg(currentPage, searchName);
+					layer.msg(result.message);
+				}
+			});
+		} else {
+			$(this).close();
+		}
+	});
 });
-
-// Reset the modal form.
 function formReset(element) {
 	$(element)[0].reset();
 	$(element).find(".form-control").removeClass("is-valid is-invalid");
 	$(element).find(".form-text").removeClass("valid-feedback invalid-feedback");
 	$(element).find(".form-text").text("");
 }
-
-// Add status color to the modal form input rows.
 function showValidationMsg(element, status, msg) {
 	$(element).removeClass("is-valid is-invalid");
 	$(element).next("span").removeClass("valid-feedback invalid-feedback");
